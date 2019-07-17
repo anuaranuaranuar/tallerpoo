03a42d4e799d0d842c9b0052f4b389c544ba50ef
anuaranuaranuar <51975464+anuaranuaranuar@users.noreply.github.com>
2019-07-17 16:48:27 -0500

    final

--- talle-poo/src/talle/poo/TallePoo.java
+++ talle-poo/src/talle/poo/TallePoo.java
@@ -16,6 +16,12 @@ public class TallePoo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        carro_4x4 carX1, carX2, carX3;
+        
+        carX1 = new carro_4x4 ("toyota","tyz-456",Integer.toString(544325),Float.toHexString(54.543),Float.toHexString((float) 54.5),"diesel","4");
+        
+        
+        
     }
     
 }
